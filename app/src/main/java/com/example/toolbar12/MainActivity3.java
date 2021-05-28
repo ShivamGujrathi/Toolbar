d86b5759b0c2c1fca241ef8cd247b5876fb4844d
ShivamGujrathi <79434419+ShivamGujrathi@users.noreply.github.com>
2021-05-28 14:32:17 +0530

    Init

---
+++ app/src/main/java/com/example/toolbar12/MainActivity3.java
@@ -0,0 +1,35 @@
+package com.example.toolbar12;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity3 extends AppCompatActivity{
+Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+        Toolbar toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        button=findViewById(R.id.button12);
+      //  button.setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+
+   // @Override
+    //public void onClick(View view) {
+      //  Intent intent= new Intent(this,MainActivity3.class);
+        //startActivity(intent);
+    //}
+    }
